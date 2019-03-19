b4f5ea594156e406963d8110476bc346b293e47d
tim <tim.taznar@gmail.com>
2019-03-18 22:00:33 -0600

    Added DEATH and Finished Shadow Colliders

---
+++ ShadowTest2D/ShadowTest2D/Assets/Scripts/Death.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    public string PlayerTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
