bad50c3e63578b55500c3809e4de59ef10b742fb
tim <tim.taznar@gmail.com>
2019-04-23 11:22:04 -0600

    Connected Main Menu

---
+++ ShadowTest2D/ShadowTest2D/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
