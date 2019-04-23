0aa49406d830508c66d63dbc9de4f3d94213d0ae
unknown <tim.taznar@gmail.com>
2019-04-23 06:09:12 -0600

    Major adjustments

--- ShadowTest2D/ShadowTest2D/Assets/MothDecalAnimator.cs
+++ ShadowTest2D/ShadowTest2D/Assets/MothDecalAnimator.cs
@@ -7,13 +7,21 @@ using UnityEngine;
 public class MothDecalAnimator : MonoBehaviour
 {
     public Decal ShadowPlayer;
-    public float FPS;
-    public ShadowAnimation RunningAnimation;
+    public ShadowAnimation RunningRightAnimation;
+    public ShadowAnimation RunningLeftAnimation;
+    public ShadowAnimation JumpRightAnimtaion;
+    public ShadowAnimation JumpLeftAnimation;
+    public ShadowAnimation IdleRightAnimation;
+    public ShadowAnimation IdleLeftAnimation;
 
+    private float FPS;
     private int currentFrame = 0;
     private float waitTime;
+    private string current;
 
     private bool _animating;
+
+    private bool _movingright;
 
     private void Start()
     {
@@ -25,14 +33,37 @@ public class MothDecalAnimator : MonoBehaviour
         {
             waitTime = 1 / FPS;
         }
-
-        StartCoroutine("Run");
+        current = "Idle";
+        PlayIdle(true);
     }
 
-    public void PlayRun()
+    public string getCurrent()
+    {
+        return current;
+    }
+    public bool isRight()
+    {
+        return _movingright;
+    }
+
+    public void calcWaitTime(float fps)
+    {
+        if (fps == 0)
+        {
+            waitTime = .05f;
+        }
+        else
+        {
+            waitTime = 1 / fps;
+        }
+    }
+
+    public void PlayRun(bool movingright)
     {
         if(!_animating)
         {
+            current = "Run";
+            _movingright = movingright;
             _animating = true;
             currentFrame = 0;
             StartCoroutine("Run");
@@ -41,16 +72,30 @@ public class MothDecalAnimator : MonoBehaviour
 
     public void StopRun()
     {
+        Debug.Log("STOP RUN");
         StopCoroutine("Run");
         _animating = false;
     }
 
     private IEnumerator Run()
     {
-        while(true)
+        ShadowAnimation frames;
+        if(_movingright)
         {
-            ShadowPlayer.Sprite = RunningAnimation.AnimationFrames[currentFrame];
-            if(currentFrame == RunningAnimation.AnimationFrames.Length -1)
+            frames = RunningRightAnimation;
+        }
+        else
+        {
+            frames = RunningLeftAnimation;
+        }
+
+        calcWaitTime(frames.FPS);
+        while(_animating)
+        {
+            Debug.Log("Running on Frame: " + currentFrame.ToString());
+            Debug.Log("MaxFrames = " + frames.AnimationFrames.Length);
+            ShadowPlayer.Sprite = frames.AnimationFrames[currentFrame];
+            if(currentFrame == frames.AnimationFrames.Length -1)
             {
                 currentFrame = 0;
             }
@@ -62,8 +107,99 @@ public class MothDecalAnimator : MonoBehaviour
         }
     }
 
+    public void PlayJump(bool movingright)
+    {
+        if (!_animating)
+        {
+            current = "Jump";
+            _movingright = movingright;
+            _animating = true;
+            currentFrame = 0;
+            StartCoroutine("Jump");
+        }
+    }
+
+    public IEnumerator Jump()
+    { 
+        ShadowAnimation frames;
+        if (_movingright)
+        {
+            frames = JumpRightAnimtaion;
+        }
+        else
+        {
+            frames = JumpLeftAnimation;
+        }
+        calcWaitTime(frames.FPS);
+        while (_animating)
+        {
+            ShadowPlayer.Sprite = frames.AnimationFrames[currentFrame];
+            if (currentFrame == frames.AnimationFrames.Length - 1)
+            {
+                currentFrame = 0;
+            }
+            else
+            {
+                currentFrame++;
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    public void StopJump()
+    {
+        StopCoroutine("Jump");
+        _animating = false;
+    }
+
+    public void PlayIdle(bool movingright)
+    {
+        if (!_animating)
+        {
+            current = "Idle";
+            _movingright = movingright;
+            _animating = true;
+            currentFrame = 0;
+            StartCoroutine("Idle");
+        }
+    }
+
+    public IEnumerator Idle()
+    {
+        ShadowAnimation frames;
+        if (_movingright)
+        {
+            frames = IdleRightAnimation;
+        }
+        else
+        {
+            frames = IdleLeftAnimation;
+        }
+        calcWaitTime(frames.FPS);
+        while (_animating)
+        {
+            ShadowPlayer.Sprite = frames.AnimationFrames[currentFrame];
+            if (currentFrame == frames.AnimationFrames.Length - 1)
+            {
+                currentFrame = 0;
+            }
+            else
+            {
+                currentFrame++;
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    public void StopIdle()
+    {
+        StopCoroutine("Idle");
+        _animating = false;
+    }
+
     public void StopAll()
     {
         StopAllCoroutines();
+        _animating = false;
     }
 }
--- ShadowTest2D/ShadowTest2D/Assets/Scripts/PerceptionController.cs
+++ ShadowTest2D/ShadowTest2D/Assets/Scripts/PerceptionController.cs
@@ -282,12 +282,10 @@ public class PerceptionController : MonoBehaviour
 
     private IEnumerator TickTime()
     {
-        Debug.Log("HERE");
         while(!_outOfTime)
         {
             yield return new WaitForSeconds(_tickAmount);
             _currentTime -= TickAmount;
-            Debug.Log(_currentTime);
 
             if (_currentTime <= 0f)
             {
